11be79fb5fbb6b257fee877222f667346b61d33b
Bob Rockers <rockers@okstate.edu>
2019-02-06 19:49:39 -0600

    Using swithes and do-while loops.

--- main.cpp
+++ main.cpp
@@ -1,37 +1,47 @@
 #include <iostream>
 using namespace std;
 
+int num1 = 0;
+int num2 = 0;
+
+void printoptions(){
+	cout << "Enter your first number: ";
+	cin >> num1;
+	cout << "Enter your second number: ";
+	cin >> num2;
+}
+
 int main(){	
 
 	char op = 'A';
 	char again = 'Y';
-	int num1 = 0;
-	int num2 = 0;
 
 	cout << "Our C++ Calculator\n";
 	
-	while('n' != again){
+	do {
 		cout << "(A)dd or (S)ubtract? ";
 		cin >> op;
 		
-		cout << "Enter your first number: ";
-		cin >> num1;
-		cout << "Enter your second number: ";
-		cin >> num2;
-		
-		if(('A' == op) || ('a' == op)){
-			cout << endl << num1 << " + " 
-				<< num2 << " = " << (num1 + num2) << endl;
-		} else if (('S' == op) || ('s' == op)) {
-			cout << endl << num1 << " - " 
-				<< num2 << " = " << (num1 - num2) << endl;
-		} else {
-			cout << op << " is not a valid option..." << endl;
+		switch ( op ) {
+			case 'a':
+			case 'A':
+				printoptions();
+				cout << endl << num1 << " + " 
+					<< num2 << " = " << (num1 + num2) << endl;
+				break;
+			case 's':
+			case 'S':
+				printoptions();
+				cout << endl << num1 << " - " 
+					<< num2 << " = " << (num1 - num2) << endl;
+				break;
+			default:
+				cout << op << " is not a valid option..." << endl;
 		}
 		
 		cout << "\nDo you want to use Calculator again (Y/n)? ";
 		cin >> again;
-	}
+	} while ('n' != again);
 	
 	return 0; 
 }
