fce7d448d2e449b837bb71699602d13ff1837803
Bob Rockers <rockers@okstate.edu>
2019-02-06 19:09:05 -0600

    Actually adding things now

--- main.cpp
+++ main.cpp
@@ -5,13 +5,24 @@ int main(){
 
 	char op = 'A';
 	char again = 'Y';
+	int num1 = 0;
+	int num2 = 0;
 
 	cout << "Our C++ Calculator\n";
 	
 	while('n' != again){
-		cout << "(A)dd or (S)ubtract? ";
+		cout << "(A)dd? ";
 		cin >> op;
-		cout << " Do you want to use Calculator again (Y/n)? ";
+		
+		cout << "Enter your first number: ";
+		cin >> num1;
+		cout << "Enter your second number: ";
+		cin >> num2;
+		
+		cout << endl << num1 << " + " 
+			<< num2 << " = " << (num1 + num2) << endl;
+		
+		cout << "\nDo you want to use Calculator again (Y/n)? ";
 		cin >> again;
 	}
 	
